aa11da942990d79f5abe18997752364eb0d2f032
pritam khaplee <pritamkhaple8585@gmail.com>
2024-05-25 16:55:39 +0530

    all api & login page FE BE complete

--- backend/routes/tweetRoute.js
+++ backend/routes/tweetRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createTweet, deleteTweet, likeOrDislike } from "../controllers/tweetController.js";
+import { createTweet, deleteTweet, getAllTweets, getFollowingTweets, likeOrDislike } from "../controllers/tweetController.js";
 import isAunthenticated from "../config/auth.js";
 
 const router  = express.Router();
@@ -8,6 +8,8 @@ router.route("/create").post(isAunthenticated,createTweet);
 router.route("/create").post(isAunthenticated,createTweet);
 router.route("/delete/:id").delete(isAunthenticated,deleteTweet);
 router.route("/like/:id").put(isAunthenticated,likeOrDislike)
+router.route("/allTweets/:id").get(isAunthenticated,getAllTweets)
+router.route("/followingtweets/:id").get(isAunthenticated,getFollowingTweets)
 
 
 
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Login, Logout, Register, bookmarks, getMyProfile } from "../controllers/userController.js";
+import { Login, Logout, Register, bookmarks, follow, getMyProfile, getOtherUsers, unfollow } from "../controllers/userController.js";
 import isAunthenticated from "../config/auth.js";
 
 const router  = express.Router();
@@ -9,6 +9,9 @@ router.route("/login").post(Login);
 router.route("/logout").get(Logout);
 router.route("/bookmark/:id").put(isAunthenticated,bookmarks)
 router.route("/profile/:id").get(isAunthenticated,getMyProfile)
+router.route("/otherUser/:id").get(isAunthenticated,getOtherUsers)
+router.route("/follow/:id").post(isAunthenticated,follow)
+router.route("/unfollow/:id").post(isAunthenticated,unfollow)
 
 
 export default router;
