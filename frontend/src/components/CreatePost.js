8785a99cf1d807499cc4d1e5167fbd3d8600ff36
pritamkhaple <pritamkhaple8585@gmail.com>
2024-06-04 18:59:19 +0530

    commit

--- frontend/src/components/CreatePost.js
+++ frontend/src/components/CreatePost.js
@@ -5,13 +5,14 @@ import { FaRegImage } from "react-icons/fa";
 import {TWEET_API_END_POINT} from  "../utils/constant";
 import toast from 'react-hot-toast';
 import { useDispatch, useSelector } from 'react-redux';
-import { getRefresh } from '../redux/tweetSlice';
+import { getAllTweets, getIsActive, getRefresh } from '../redux/tweetSlice';
 
 
 export default function CreatePost() {
 
     const  [description, setDescription] = useState("");
     const {user} = useSelector(store=>store.user)
+    const {isActive} = useSelector(store => store.tweet);
     const dispatch = useDispatch();
 
     const submitHandler = async () =>{
@@ -33,12 +34,20 @@ export default function CreatePost() {
         setDescription("");
     }
 
+    const forYouHandler = () =>{
+        dispatch(getIsActive(true))
+    }
+
+    const followingHandler = () =>{
+        dispatch(getIsActive(false))
+    }
+
   return (
     <div className='w-[100%]'>
         <div className=''>
         <div className='flex justify-between border-b border-gray-100 mb-2'>
-            <div className='w-full '><h1 className='px-4 py-3 font-semibold text-gray-600 text-center text-lg cursor-pointer hover:bg-gray-100'>For you</h1></div>
-            <div className='w-full '><h1 className='px-4 py-3 font-semibold text-gray-600 text-lg cursor-pointer hover:bg-gray-100 text-center'>Following</h1></div>
+            <div onClick={forYouHandler} className='w-full '><div className={`${isActive ? "border-b-4 border-blue-700": "border border-transparent"} cursor-pointer hover:bg-gray-100 text-center`}><h1 className='px-4 py-3 font-semibold text-gray-600 text-center text-lg '>For you</h1></div></div>
+            <div onClick={followingHandler} className='w-full '><div className={`${!isActive ? "border-b-4 border-blue-700": "border border-transparent"} cursor-pointer hover:bg-gray-100 text-center`}><h1 className='px-4 py-3 font-semibold text-gray-600 text-lg  text-center'>Following</h1></div></div>
         </div>
         <div className=''>
             <div className='flex items-center p-5 '>
--- frontend/src/redux/tweetSlice.js
+++ frontend/src/redux/tweetSlice.js
@@ -5,6 +5,7 @@ const tweetSlice = createSlice({
     initialState:{
         tweets:[],
         refresh:false,
+        isActive:true,
     },
     reducers:{
         getAllTweets:(state,action) =>{
@@ -12,10 +13,13 @@ const tweetSlice = createSlice({
         },
         getRefresh:(state)=>{
             state.refresh = !state.refresh
+        },
+        getIsActive:( state,action) => {
+            state.isActive = action.payload; 
         }
     }
 })
 
 
-export const {getAllTweets, getRefresh} = tweetSlice.actions;
+export const {getAllTweets, getRefresh,getIsActive} = tweetSlice.actions;
 export default tweetSlice.reducer;
