7bd256815d95044024696e7ddde2e32c7b5975e1
Pritam Khaple <110626364+pritamkhaple@users.noreply.github.com>
2024-06-06 17:52:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,34 +2,32 @@ import express from "express";
 import dotenv from "dotenv";
 import databaseConnection from "./config/database.js";
 import cookieParser from "cookie-parser";
-import userRoute from './routes/userRoute.js'
-import tweetRoute from './routes/tweetRoute.js'
-import cors from "cors"
-dotenv.config({
-    path:".env"
-})
+import userRoute from './routes/userRoute.js';
+import tweetRoute from './routes/tweetRoute.js';
+import cors from "cors";
+
+dotenv.config({ path: ".env" });
 databaseConnection();
+
 const app = express();
 
-//middlewares
-app.use(express.urlencoded({
-    extended:true
-}));
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-const corsOptions ={
-    origin:"https://twitter-clone-in-mern-frontend.vercel.app",
+
+const corsOptions = {
+    origin: "https://twitter-clone-in-mern-frontend.vercel.app",
     methods: ["POST", "GET"],
-    credentials:true
-}
+    credentials: true
+};
 app.use(cors(corsOptions));
 
-//APIs
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/tweet",tweetRoute);
+// APIs
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/tweet", tweetRoute);
 
-
-
-app.listen(process.env.PORT , () => {
-    console.log(`Server listen at port ${process.env.PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server listening at port ${PORT}`);
+});
