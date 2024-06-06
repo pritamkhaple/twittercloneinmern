15db2d35f78405bb62c9b5ae92109ab618ee6262
Pritam Khaple <110626364+pritamkhaple@users.noreply.github.com>
2024-06-06 17:18:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,8 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions ={
-    origin:"http://localhost:3000",
+    origin:"https://twitter-clone-in-mern.vercel.app",
+    methods: ["POST", "GET"],
     credentials:true
 }
 app.use(cors(corsOptions));
