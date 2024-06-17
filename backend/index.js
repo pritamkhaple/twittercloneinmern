652a600ca173c664cdd4bd8b84d0e98f2339c0ce
Pritam Khaple <110626364+pritamkhaple@users.noreply.github.com>
2024-06-17 15:27:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(cors(corsOptions));
 // Manually set Access-Control-Allow-Credentials header
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Origin", "https://twittercloneinmern-fe.vercel.app");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
