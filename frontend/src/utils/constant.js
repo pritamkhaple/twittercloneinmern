c601f17cd3f4bd41d9b396ed38802ee7fc785466
Pritam Khaple <110626364+pritamkhaple@users.noreply.github.com>
2024-06-17 13:08:55 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT = "https://twitter-clone-in-mern.vercel.app/api/v1/user"
-export const TWEET_API_END_POINT = "https://twitter-clone-in-mern.vercel.app/api/v1/tweet"
+export const USER_API_END_POINT = "https://twittercloneinmern-backend.vercel.app/api/v1/user"
+export const TWEET_API_END_POINT = "https://twittercloneinmern-backend.vercel.app/api/v1/tweet"
